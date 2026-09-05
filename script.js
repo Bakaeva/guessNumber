3dc00c7a231651f87384b0a93abb0d10a6cffe4c
Bakaeva <l_bakaeva74@mail.ru>
2026-09-05 23:49:23 +0300

    Выполнено д/з урока 6 (функции, область видимости и замыкания)

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 'use strict';
 
-//#region Functions declaration
 function isNumber(n) {
     return !isNaN(parseInt(n)) && isFinite(n);
 }
 
-function guessTheNumberWithLimit() {
+function guessTheNumber() {
     const originNumber = parseInt(Math.random() * 100 + 1); // загаданное число
     //console.log(originNumber);
-    let tryCounter = 10;
 
     function tryToGuess() {
-        if (tryCounter === 0) {
-            if (confirm('Попытки закончились, хотите сыграть еще?'))
-                guessTheNumberWithLimit();
-            return;
-        }
-
-        tryCounter -= 1;
-
         let num = prompt('Угадай число от 1 до 100:');
         if (num === null) { // нажато 'Отмена' | ESC
             alert('Игра окончена');
@@ -30,12 +20,11 @@ function guessTheNumberWithLimit() {
         } else {
             num = Number(num);
             if (num > originNumber)
-                alert('Загаданное число меньше ' + num + ', осталось попыток ' + tryCounter);
+                alert('Загаданное число меньше ' + num);
             else if (num < originNumber)
-                alert('Загаданное число больше ' + num + ', осталось попыток ' + tryCounter);
+                alert('Загаданное число больше ' + num);
             else {
-                if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?'))
-                    guessTheNumberWithLimit();
+                alert('Поздравляю, Вы угадали!!!');
                 return;
             }
         }
@@ -45,6 +34,5 @@ function guessTheNumberWithLimit() {
 
     tryToGuess();
 }
-//#endregion Functions declaration
 
-guessTheNumberWithLimit();
+guessTheNumber();
